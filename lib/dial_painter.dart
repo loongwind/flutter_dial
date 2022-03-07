11cc2ecfc3223ad813acae2c835d708210adb24e
loongwind <loongwind@gmail.com>
2022-03-07 11:04:03 +0800

    optimize and add comment

--- lib/dial_painter.dart
+++ lib/dial_painter.dart
@@ -13,17 +13,15 @@ class DialPainter extends CustomPainter{
   late final Paint _paint = _initPaint();
   late double unit ;
 
-
-  Paint _initPaint(){
-    return Paint()..isAntiAlias = true;
+  Paint _initPaint() {
+    return Paint()
+      ..isAntiAlias = true
+      ..color = Colors.white;
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    width = size.width;
-    height = size.height;
-    radius = width / 2 -20;
-    unit = radius / 15;
+    initSize(size);
 
     drawDial(canvas);
 
@@ -36,13 +34,19 @@ class DialPainter extends CustomPainter{
 
     var date = DateTime.now();
 
-
     drawHour(canvas, date);
 
     drawMinutes(canvas, date);
 
     drawSeconds(canvas, date);
 
+  }
+
+  void initSize(Size size) {
+    width = size.width;
+    height = size.height;
+    radius = width / 2 -20;
+    unit = radius / 15;
   }
 
   void drawSeconds(Canvas canvas, DateTime date) {
@@ -54,17 +58,35 @@ class DialPainter extends CustomPainter{
 
     Path secondsPath = Path();
     secondsPath.moveTo(secondsLeft, secondsTop);
-    secondsPath.addArc(Rect.fromLTWH(secondsLeft, secondsTop, 2.5 * unit, hourHalfHeight * 2), pi/2, pi);
-    secondsPath.addRRect(RRect.fromLTRBR(secondsLeft + 1 * unit, secondsTop, - 2 * unit, secondsBottom, Radius.circular(0.25 * unit)));
+
+    /// 尾部弧形
+    var rect = Rect.fromLTWH(secondsLeft, secondsTop, 2.5 * unit, hourHalfHeight * 2);
+    secondsPath.addArc(rect, pi/2, pi);
+
+    /// 尾部圆角矩形
+    var rRect = RRect.fromLTRBR(secondsLeft + 1 * unit, secondsTop, - 2 * unit, secondsBottom, Radius.circular(0.25 * unit));
+    secondsPath.addRRect(rRect);
+
+    /// 指针
     secondsPath.moveTo(- 2 * unit, - 0.125 * unit);
     secondsPath.lineTo(secondsRight, 0);
     secondsPath.lineTo(-2 * unit, 0.125 * unit);
-    secondsPath.addOval(Rect.fromLTWH(- 0.67 * unit, - 0.67 * unit, 1.33 * unit, 1.33 * unit));
+
+    /// 中心圆
+    var ovalRect = Rect.fromLTWH(- 0.67 * unit, - 0.67 * unit, 1.33 * unit, 1.33 * unit);
+    secondsPath.addOval(ovalRect);
+
     canvas.save();
     canvas.translate(width/2, height/2);
     canvas.rotate(2*pi/60 * (date.second - 15));
+
+    /// 绘制阴影
     canvas.drawShadow(secondsPath, const Color(0xFFcc0000), 0.17 * unit, true);
-    canvas.drawPath(secondsPath, _paint..color = const Color(0xFFcc0000));
+
+    /// 绘制秒针
+    _paint.color = const Color(0xFFcc0000);
+    canvas.drawPath(secondsPath, _paint);
+
     canvas.restore();
   }
 
@@ -78,55 +100,85 @@ class DialPainter extends CustomPainter{
     canvas.save();
     canvas.translate(width/2, height/2);
     canvas.rotate(2*pi/60 * (date.minute - 15 + date.second / 60));
-    canvas.drawRRect(RRect.fromLTRBR(minutesLeft, minutesTop, minutesRight, minutesBottom, Radius.circular(0.42 * unit)), _paint..color = const Color(0xFF343536));
+
+    /// 绘制分针
+    var rRect = RRect.fromLTRBR(minutesLeft, minutesTop, minutesRight, minutesBottom, Radius.circular(0.42 * unit));
+    _paint.color = const Color(0xFF343536);
+    canvas.drawRRect(rRect, _paint);
+
     canvas.restore();
   }
 
-  double drawHour(Canvas canvas, DateTime date) {
+  void drawHour(Canvas canvas, DateTime date) {
 
     double hourHalfHeight = 0.4 * unit;
     double hourRectRight =   7 * unit;
+
     Path hourPath = Path();
+    /// 添加矩形 时针主体
     hourPath.moveTo(0 - hourHalfHeight, 0 - hourHalfHeight);
     hourPath.lineTo(hourRectRight, 0 - hourHalfHeight);
     hourPath.lineTo(hourRectRight, 0 + hourHalfHeight);
     hourPath.lineTo(0 - hourHalfHeight, 0 + hourHalfHeight);
 
+    /// 时针箭头尾部弧形
     double offsetTop = 0.5 * unit;
     double arcWidth = 1.5 * unit;
     double arrowWidth = 2.17 * unit;
     double offset = 0.42 * unit;
-    hourPath.addArc(Rect.fromLTWH(hourRectRight - offset, 0 - hourHalfHeight - offsetTop, arcWidth, hourHalfHeight * 2 + offsetTop * 2), pi/2, pi);
+    var rect = Rect.fromLTWH(hourRectRight - offset, 0 - hourHalfHeight - offsetTop, arcWidth, hourHalfHeight * 2 + offsetTop * 2);
+    hourPath.addArc(rect, pi/2, pi);
+    /// 时针箭头
     hourPath.moveTo(hourRectRight - offset + arcWidth/2, 0 - hourHalfHeight - offsetTop);
     hourPath.lineTo(hourRectRight - offset + arcWidth/2 + arrowWidth, 0);
     hourPath.lineTo(hourRectRight - offset + arcWidth/2, 0 + hourHalfHeight + offsetTop);
     hourPath.close();
+
     canvas.save();
     canvas.translate(width/2, height/2);
-
     canvas.rotate(2*pi/60*((date.hour - 3 + date.minute / 60 + date.second/60/60) * 5 ));
-    canvas.drawPath(hourPath, _paint..color = const Color(0xFF232425));
+    ///绘制
+    _paint.color = const Color(0xFF232425);
+    canvas.drawPath(hourPath, _paint);
     canvas.restore();
-    return hourHalfHeight;
   }
 
   void drawCenter(Canvas canvas) {
-
-    var radialGradient = ui.Gradient.radial(Offset(width/2, height/2), radius, [
-      const Color.fromARGB(255, 200,200,200),
-      const Color.fromARGB(255,190,190,190),
-      const Color.fromARGB(255,130,130,130),
+    var radialGradient =
+        ui.Gradient.radial(Offset(width / 2, height / 2), radius, [
+      const Color.fromARGB(255, 200, 200, 200),
+      const Color.fromARGB(255, 190, 190, 190),
+      const Color.fromARGB(255, 130, 130, 130),
     ], [0, 0.9, 1.0]);
-    canvas.drawCircle(Offset(width/2, height/2), 2 * unit, _paint..shader = radialGradient..style = PaintingStyle.fill);
+
+    /// 底部背景
+    _paint
+      ..shader = radialGradient
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(
+        Offset(width/2, height/2), 2 * unit, _paint);
 
 
-    canvas.drawCircle(Offset(width/2, height/2), 0.8 * unit, _paint..shader = null..style = PaintingStyle.fill..color = const Color(0xFF121314));
+    /// 顶部圆点
+    _paint
+      ..shader = null
+      ..style = PaintingStyle.fill
+      ..color = const Color(0xFF121314);
+    canvas.drawCircle(Offset(width/2, height/2), 0.8 * unit, _paint);
   }
 
   void drawDial(ui.Canvas canvas) {
-    var gradient = ui.Gradient.linear(Offset(width/2, height/2 - radius,), Offset(width/2, height/2 + radius), [const Color(0xFFF9F9F9), const Color(0xFF666666)]);
-    canvas.drawCircle(Offset(width/2, height/2), radius, _paint..color = Colors.white..shader = gradient);
+    /// 绘制一个线性渐变的圆
+    var gradient = ui.Gradient.linear(
+        Offset(width/2, height/2 - radius,),
+        Offset(width/2, height/2 + radius),
+        [const Color(0xFFF9F9F9), const Color(0xFF666666)]);
 
+    _paint.shader = gradient;
+    _paint.color = Colors.white;
+    canvas.drawCircle(Offset(width/2, height/2), radius, _paint);
+
+    /// 绘制一层径向渐变的圆
     var radialGradient = ui.Gradient.radial(Offset(width/2, height/2), radius, [
       const Color.fromARGB(255, 246, 248, 249),
       const Color.fromARGB(255, 229, 235, 238),
@@ -135,16 +187,26 @@ class DialPainter extends CustomPainter{
     ], [0, 0.92, 0.93, 1.0]);
 
     var shadowRadius = radius -  0.8 * unit;
-    canvas.drawCircle(Offset(width/2, height/2), radius -  0.3 * unit, _paint..color = Colors.white..shader = radialGradient);
+    _paint.shader = radialGradient;
+    canvas.drawCircle(Offset(width/2, height/2), radius -  0.3 * unit, _paint);
 
-    canvas.drawCircle(Offset(width/2, height/2), shadowRadius - 0.2 * unit, _paint..color = const Color.fromARGB(33, 0, 0, 0)..shader = null..style = PaintingStyle.stroke..strokeWidth = 0.1 * unit);
+    /// 绘制一层border
+    _paint
+      ..color = const Color.fromARGB(33, 0, 0, 0)
+      ..shader = null
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 0.1 * unit;
+    canvas.drawCircle(Offset(width/2, height/2), shadowRadius - 0.2 * unit, _paint);
 
+    ///绘制阴影
     Path path = Path();
     path.moveTo(width/2, height/2);
-    path.addOval(Rect.fromLTRB(width/2 - shadowRadius, height/2 - shadowRadius, width/2+shadowRadius, height /2 +shadowRadius));
+    var rect = Rect.fromLTRB(width/2 - shadowRadius, height/2 - shadowRadius, width/2+shadowRadius, height /2 +shadowRadius);
+    path.addOval(rect);
     canvas.drawShadow(path, const Color.fromARGB(51, 0, 0, 0), 1 * unit, true);
   }
 
+  /// 绘制刻度
   void drawCalibration(ui.Canvas canvas) {
     double dialCanvasRadius = radius -  0.8 * unit;
     canvas.save();
@@ -166,6 +228,7 @@ class DialPainter extends CustomPainter{
     canvas.restore();
   }
 
+  /// 绘制刻度上的值 3 6 9 12
   void drawText(ui.Canvas canvas) {
     double dialCanvasRadius = radius -  0.8 * unit;
     var textPainter = TextPainter(
